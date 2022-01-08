64873ce85b955f53331a5971836b9f11c144a63c
Aniket <aniketdhokane2071@gmail.com>
2022-01-08 23:19:18 +0530

    variables and constants in Go

--- main.go
+++ main.go
@@ -3,6 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World!") // we can write the print function with the fmt
+	var conferenceName = "Go conference"
+
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to ", conferenceName, " booking application") // we can write the print function with the fmt
+	fmt.Println("We have total of ", conferenceTickets, " tickets and", remainingTickets, "are still available")
+	fmt.Println("Get you tickets to attend")
 
 }
+
+// the error comes in go when we import package or declare variable but dont use it.
+// we use const to the value that does not change
