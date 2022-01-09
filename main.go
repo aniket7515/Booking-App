e4d3e32742fc88e7349229e0f00ca450c498e973
Aniket <aniketdhokane2071@gmail.com>
2022-01-10 00:12:17 +0530

    Data Types in Go

--- main.go
+++ main.go
@@ -8,11 +8,23 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
+	fmt.Printf("ConferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceName, remainingTickets, conferenceName)
+
 	fmt.Printf("Welcome to %v booking application\n", conferenceName) // we can write the print function with the fmt
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get you tickets to attend")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
 
 // the error comes in go when we import package or declare variable but dont use it.
 // we use const to the value that does not change
+
+// 34:11
